d3f868e930e9ffbd850bb8e9f4afa589f32fbaca
Dave Price <d.price@elifesciences.org>
2020-03-06 11:31:10 +0000

    fetch second number in sequence

--- main.test.js
+++ main.test.js
@@ -1,7 +1,14 @@
-function getNumberAt(postion) {
+function getNumberAt(position) {
+  if (position == 1) {
+    return 1;
+  }
   return 0;
 };
 
 test('first number in sequence is zero', () => {
   expect(getNumberAt(0)).toBe(0);
+});
+
+test('Second number in sequence is one', () => {
+  expect(getNumberAt(1)).toBe(1);
 });
